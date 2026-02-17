7853fde374283d13fccb1369808687ea5fa0bc91
fenil matt fenil matt <ffenilmatt@gmail.com>
2026-02-17 15:31:20 +0530

    metainskin

--- app/skintreatment/page.tsx
+++ app/skintreatment/page.tsx
@@ -12,8 +12,9 @@ import { TestimonialsSectionskin } from '@/components/testimonialskin';
 import WhyTrustSectionskin from '@/components/whyskin';
 // import { WhyChooseSection } from '@/components/TrustSectionhair';
 import React, { useState, useEffect } from 'react';
+import Script from 'next/script';
 
-export default function Hair() {
+export default function Skin() {
   const [scrollY, setScrollY] = useState(0);
 
   useEffect(() => {
@@ -23,25 +24,59 @@ export default function Hair() {
   }, []);
 
   return (
-    <div className="min-h-screen bg-white overflow-x-hidden">
-      {/* <Navigation scrollY={scrollY} /> */}
-      <section id='home'>
-      <SkinheroSectionhair />
-      </section>
-       <BeforeAfterGalleryskin />
-      <SkinvideoTestimonials />
-      <section id='treatment'>
-      <Skintreatment />
-      </section>
-      <SkinCombinedSectionhair />
-      <section id='doctor'>
-      <Skindoctor />
-      </section>
-      <WhyTrustSectionskin />
-      <TestimonialsSectionskin />
-      <FAQSectionskin />
-      <ClinicLocationMaphair />
-      <Footerskin />
-    </div>
+    <>
+      {/* Meta Pixel Code */}
+      <Script
+        id="meta-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '4155341721462818');
+            fbq('track', 'PageView');
+          `
+        }}
+      />
+      
+      {/* Fallback noscript image for Meta Pixel */}
+      <noscript>
+        <img 
+          height="1" 
+          width="1" 
+          style={{ display: 'none' }}
+          src="https://www.facebook.com/tr?id=4155341721462818&ev=PageView&noscript=1"
+          alt=""
+        />
+      </noscript>
+      {/* End Meta Pixel Code */}
+
+      <div className="min-h-screen bg-white overflow-x-hidden">
+        {/* <Navigation scrollY={scrollY} /> */}
+        <section id='home'>
+          <SkinheroSectionhair />
+        </section>
+        <BeforeAfterGalleryskin />
+        <SkinvideoTestimonials />
+        <section id='treatment'>
+          <Skintreatment />
+        </section>
+        <SkinCombinedSectionhair />
+        <section id='doctor'>
+          <Skindoctor />
+        </section>
+        <WhyTrustSectionskin />
+        <TestimonialsSectionskin />
+        <FAQSectionskin />
+        <ClinicLocationMaphair />
+        <Footerskin />
+      </div>
+    </>
   );
 }
