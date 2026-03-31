0007b2e3cafdea8f3857a0fc92da081c7c9545ce
crowddevelopers08-arch <crowddevelopers08@gmail.com>
2026-03-31 15:34:27 +0530

    new project anti-angingjygtfs

--- components/anti-agings/thank-page.tsx
+++ components/anti-agings/thank-page.tsx
@@ -1,8 +1,14 @@
 'use client'
 
-import React from "react";
+import React, { useEffect } from "react";
+
+declare function gtag(...args: unknown[]): void;
 
 export default function ThankYou() {
+  useEffect(() => {
+    gtag('event', 'conversion', { send_to: 'AW-11327193954/8nIoCN_f9ZIcEOKGnZkq' });
+  }, []);
+
   return (
     <div
       style={{
